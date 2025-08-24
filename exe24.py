ed261c26a352cd2c879a11b2c2e06455ffcba626
brunomsmcarvalho <brunomatoscarvalho@hotmail.com>
2025-08-24 12:00:55 +0100

    Update exe24.py

--- exe24.py
+++ exe24.py
@@ -1,3 +1,4 @@
-#crie um progra,a que leia o nome de uma cidade e diga se ela comeca ou nao com o nome "santo"
+#crie um programa que leia o nome de uma cidade e diga se ela comeca ou nao com o nome "santo"
 cidade = str(input('Introduza a cidade onde voce nasceu: ').strip())
 print(cidade[:5].upper() == 'SANTO')
+
