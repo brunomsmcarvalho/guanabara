239a06b4aaf050f3535ca1fd53004fd62f6a7bb4
brunomsmcarvalho <brunomatoscarvalho@hotmail.com>
2025-09-01 14:31:29 +0100

    Update exe39.py

--- exe39.py
+++ exe39.py
@@ -1,6 +1,6 @@
-#faca um programa que leia o ano de nascvimento de um jovem e informe de acordo com a sua idade
-#se ele ainda vai se alistar ao servico militar, se 'e hora de se alistar,se ja passou do tempo do alistamento
-#seu programa tb devera mostrar o tempo que falta ou qye passou do prazo
+#faca um programa que leia o ano de nascimento de um jovem e informe de acordo com a sua idade
+#se ele ainda vai se alistar ao servico militar, se é hora de se alistar, se ja passou do tempo do alistamento.
+#Seu programa tb devera mostrar o tempo que falta ou que passou do prazo
 from datetime import date
 nascimento = int(input('Indique o ano em que nasceu: '))
 atual = date.today().year
@@ -21,3 +21,4 @@ else:
     print('Ainda nao esta na hora de se alistar, faltam {} anos!!'.format(idd_inferior))
     ano = atual + idd_inferior
     print('Seu alistamento ser`a em {}'.format(ano))
+
